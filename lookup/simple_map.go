7e8607cf29140d2ea7f94654bd26278bc12dbb83
Anthony Lapenna <lapenna.anthony@gmail.com>
2018-07-12 08:53:06 +0200

    feat(lookup): add simple map lookup

---
+++ lookup/simple_map.go
@@ -0,0 +1,23 @@
+package lookup
+
+import (
+	"fmt"
+
+	"github.com/portainer/libcompose/config"
+)
+
+// MapLookup is a structure that implements the project.EnvironmentLookup interface.
+// It holds a map of variables where to lookup environment values.
+type MapLookup struct {
+	vars map[string]string
+}
+
+// Lookup creates a string slice of string containing a "docker-friendly" environment string
+// in the form of 'key=value'. It gets environment values from the vars map.
+func (l *MapLookup) Lookup(key string, config *config.ServiceConfig) []string {
+	ret := l.vars[key]
+	if ret == "" {
+		return []string{}
+	}
+	return []string{fmt.Sprintf("%s=%s", key, ret)}
+}
---
+++ lookup/simple_map_test.go
@@ -0,0 +1,26 @@
+package lookup
+
+import (
+	"testing"
+)
+
+func TestMapLookup(t *testing.T) {
+	mapLookup := &MapLookup{
+		vars: map[string]string{"VAR": "VALUE"},
+	}
+
+	envs := mapLookup.Lookup("VAR", nil)
+	if len(envs) != 1 {
+		t.Fatalf("Expected envs to contains one element, but was %v", envs)
+	}
+
+	envs = mapLookup.Lookup("var", nil)
+	if len(envs) != 0 {
+		t.Fatalf("Expected envs to be empty, but was %v", envs)
+	}
+
+	envs = mapLookup.Lookup("DOES_NOT_EXIST", nil)
+	if len(envs) != 0 {
+		t.Fatalf("Expected envs to be empty, but was %v", envs)
+	}
+}
